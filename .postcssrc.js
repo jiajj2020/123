02626931569a5005cecfdc7837309fff9de2d3f7
jiajj2020 <767425723@qq.com>
2020-08-01 15:34:23 +0800

    waste

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  },
+  configureWebpack: {
+    externals: {
+      'T': 'T', // 天地图
+    },
+  },
+}
