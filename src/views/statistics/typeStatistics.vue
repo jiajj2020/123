b51c9998b0b2268e49fb1292d60f14a3aeab065f
zyy0831 <798206656@qq.com>
2020-08-05 00:01:38 +0800

    jia update

--- src/views/statistics/typeStatistics.vue
+++ src/views/statistics/typeStatistics.vue
@@ -5,9 +5,23 @@
         <el-row style="padding-top: 10px">
           <el-col :xs="24" :sm="24" :md="11" :lg="5" :xl="5">
             <el-form-item class="main-text" label="类型统计 :" label-width="110px">
-              <el-cascader v-model="values" :options="options" @change="getChartData">
+              <el-cascader v-model="values" :options="options" @change="handleChange">
+                <!-- <el-cascader v-model="values" :options="options" @change="getChartData"> -->
               </el-cascader>
             </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="11" :lg="5" :xl="5">
+            <el-form-item class="main-text" label="月份 :" label-width="110px">
+              <el-date-picker v-model="value1" type="month" placeholder="选择月">
+              </el-date-picker>
+              <!-- <el-date-picker v-model="value1" type="monthrange" range-separator="至" start-placeholder="开始月份"
+              end-placeholder="结束月份" >
+              </el-date-picker>  -->
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="12" :lg="5" :xl="5" style="padding-top: 4px;text-align: right">
+            <el-button type="primary" size="small" @click="getChartData()" :loading="staLoading">查询
+            </el-button>
           </el-col>
           <!-- <el-col :xs="24" :sm="24" :md="13" :lg="7" :xl="7">
                         <el-form-item style="text-align: center;margin-bottom: 10px;">
@@ -17,20 +31,18 @@
                     </el-col> -->
         </el-row>
       </el-form>
-      <el-row id='1' v-if="values[0]==='cunliangpaicha'">
-        <el-col :xs="24" :sm="24" :md="11" :lg="11">
-          <div id="main" style="height:350px"></div>
-        </el-col>
-        <el-col :offset="2" :xs="24" :sm="24" :md="11" :lg="11">
-          <div id="main3" style="height: 350px;"></div>
-        </el-col>
-        <!-- <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+      <el-col :xs="12" :sm="12" :md="11" :lg="11">
+        <div id="main" style="height:700px" ></div>
+      </el-col>
+      <el-col  :xs="24" :sm="11" :md="11" :lg="11">
+        <div id="main3" style="height: 700px;" ></div>
+      </el-col>
+      <!-- <el-col :offset="12" :xs="12" :sm="11" :md="11" >
+        <div id="main4" style="height: 700px;" v-if="values[0]==='cunliangpaicha'"></div>
+      </el-col> -->
+      <!-- <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
                 <div id="main2" style="height: 400px;"></div>
             </el-col> -->
-      </el-row>
-      <el-row id='2' v-if="true">
-        11111111111111
-      </el-row>
     </div>
   </div>
 
@@ -38,6 +50,7 @@
 
 <script>
   import echarts from 'echarts'
+
   export default {
     name: '',
     data() {
@@ -55,46 +68,106 @@
         }, {
           value: 'ziyuanliyong',
           label: '资源利用',
-        }]
+        }],
+        value1: '',
       }
     },
     methods: {
-      drawPie1(jdakdhjahj) {
-        console.log("1111");
-        this.charts = echarts.init(document.getElementById(jdakdhjahj))
-        var hdjahdjahjdi = {
-          angleAxis: {},
-          radiusAxis: {
-            type: 'category',
-            data: ['周一'],
-            z: 10
+      drawPie1(id) {
+        this.charts = echarts.init(document.getElementById(id))
+        this.charts.setOption({
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#6a7985'
+              }
+            }
           },
-          polar: {},
-          series: [{
-            type: 'bar',
-            data: [1, 2, 3, 4],
-            coordinateSystem: 'polar',
-            name: 'A',
-            stack: 'a'
-          }, {
-            type: 'bar',
-            data: [2, 4, 6, 8],
-            coordinateSystem: 'polar',
-            name: 'B',
-            stack: 'a'
-          }, {
-            type: 'bar',
-            data: [1, 2, 3, 4],
-            coordinateSystem: 'polar',
-            name: 'C',
-            stack: 'a'
+          title: {
+            text: '2019年11月-2020年5月平顶山市建筑垃圾排查情况',
+            textStyle: {
+              fontWeight: 'normal',
+              fontSize: 18, //字体大小
+              color: '#fff'
+            },
+            left: 'center'
+          },
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '2%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [{
+            axisLine: {
+              lineStyle: {
+                color: '#fff', // 颜色
+                width: 3 // 粗细
+              }
+            },
+            type: 'category',
+            boundaryGap: false,
+            data: ['2019.11', '2019.12', '2020.01', '2020.02', '2020.03', '2020.04', '2020.05']
           }],
-          legend: {
-            show: true,
-            data: ['A', 'B', 'C']
-          }
-        }
-        this.charts.setOption(hdjahdjahjdi)
+          yAxis: [{
+            axisLine: {
+              lineStyle: {
+                color: '#fff', // 颜色
+                width: 3 // 粗细
+              }
+            },
+            type: 'value'
+          }],
+          series: [{
+              name: '装修垃圾',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+              name: '拆除垃圾',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+              name: '工程泥浆',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [150, 232, 201, 154, 190, 330, 410]
+            },
+            {
+              name: '工程垃圾',
+              type: 'line',
+              stack: '总量',
+              areaStyle: {},
+              data: [320, 332, 301, 334, 390, 330, 320]
+            },
+            {
+              name: '工程渣土',
+              type: 'line',
+              stack: '总量',
+              label: {
+                normal: {
+                  show: true,
+                  position: 'top'
+                }
+              },
+              areaStyle: {},
+              data: [820, 932, 901, 934, 1290, 1330, 1320]
+            }
+          ]
+        })
       },
       drawPie2(id) {
         this.charts = echarts.init(document.getElementById(id))
@@ -197,7 +270,7 @@
         if (this.values == "cunliangpaicha") {
           this.$nextTick(() => {
             this.drawPie1('main');
-            this.drawPie2('main3');
+            this.drawPie2('main3')
           });
         } else if (this.values == "sheshijianshe") {
           this.$nextTick(() => {
@@ -226,7 +299,16 @@
   .el-main {
     border: 0;
   }
-
+    .el-main4 {
+    border: 0;
+  }
+  .main4-text {
+    color: #fff !important;
+    font-family: Georgia, serif;
+    font-weight: bold;
+    font-size: 15px;
+    text-align: right;
+  }
   .main-text {
     color: #fff !important;
     font-family: Georgia, serif;
